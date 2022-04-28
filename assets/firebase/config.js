51bc1bc67beeddd73a47fe12a97c564e60175ba3
sahil-777 <turbothunderboltz@gmail.com>
2022-04-29 01:01:35 +0530

    configured firebase

---
+++ assets/firebase/config.js
@@ -0,0 +1,14 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDytHHRamh8k1Gp2NLUslBsjrvmW9gG_6k",
+    authDomain: "irrigation-monitoring-ioe.firebaseapp.com",
+    databaseURL: "https://irrigation-monitoring-ioe-default-rtdb.firebaseio.com",
+    projectId: "irrigation-monitoring-ioe",
+    storageBucket: "irrigation-monitoring-ioe.appspot.com",
+    messagingSenderId: "393323829047",
+    appId: "1:393323829047:web:fc528524979e19ebd46cb2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
